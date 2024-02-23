482d75dd1743ac6b87556f276e739e8786f9d7e1
IvanReinov <157304113+IvanReinov@users.noreply.github.com>
2024-02-23 21:54:19 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,467 @@
+'use strict';
+
+const listImages = [
+    {
+        src: 'заставканачальная.jpg',
+        nameImg: '1',
+    },
+    {
+        src: 'сцена1.jpg',
+        nameImg: '2',
+    },
+    {
+        src: 'сцена2.jpg',
+        nameImg: '2',
+    },
+    {
+        src: 'сцена3.jpg',
+        nameImg: '2',
+    },
+    {
+        src: 'сцена4.jpg',
+        nameImg: '2',
+    },
+    {
+        src: 'сцена5.png',
+        nameImg: '2',
+    },
+    {
+        src: 'сцена6.jpg',
+        nameImg: '2',
+    },
+    {
+        src: 'сцена7.jpg',
+        nameImg: '2',
+    },
+    {
+        src: 'сцена8.jpg',
+        nameImg: '2',
+    },
+    {
+        src: 'сцена9.png',
+        nameImg: '2',
+    },
+    {
+        src: 'сцена10.jpg',
+        nameImg: '2',
+    },
+    {
+        src: 'сцена11.jpg',
+        nameImg: '2',
+    },
+    {
+        src: 'сцена12.jpg',
+        nameImg: '2',
+    },
+    {
+        src: 'сцена13.png',
+        nameImg: '2',
+    },
+    {
+        src: 'сцена14.jpg',
+        nameImg: '2',
+    },
+    {
+        src: 'сцена15.jpg',
+        nameImg: '2',
+    },
+    {
+        src: 'сцена16.jpg',
+        nameImg: '2',
+    },
+    {
+        src: 'сцена17.png',
+        nameImg: '2',
+    },
+    {
+        src: 'сцена18.jpg',
+        nameImg: '2',
+    },
+    {
+        src: 'сцена19.jpg',
+        nameImg: '2',
+    },
+    {
+        src: 'сцена20.jpg',
+        nameImg: '2',
+    },
+    {
+        src: 'сцена21.png',
+        nameImg: '2',
+    },
+    {
+        src: 'сцена22.jpg',
+        nameImg: '2',
+    },
+    {
+        src: 'сцена23.jpg',
+        nameImg: '2',
+    },
+    {
+        src: 'сцена24.jpg',
+        nameImg: '2',
+    },
+    {
+        src: 'сцена25.png',
+        nameImg: '2',
+    },
+    {
+        src: 'сцена26.jpg',
+        nameImg: '2',
+    },
+    {
+        src: 'сцена27.jpg',
+        nameImg: '2',
+    },
+    {
+        src: 'сцена28.jpg',
+        nameImg: '2',
+    },
+    {
+        src: 'сцена29.png',
+        nameImg: '2',
+    },
+    {
+        src: 'сцена30.jpg',
+        nameImg: '2',
+    },
+    {
+        src: 'сцена31.jpg',
+        nameImg: '2',
+    },
+    {
+        src: 'сцена32.jpg',
+        nameImg: '2',
+    },
+    {
+        src: 'сцена33.png',
+        nameImg: '2',
+    },
+    {
+        src: 'сцена34.jpg',
+        nameImg: '2',
+    },
+    {
+        src: 'сцена35.jpg',
+        nameImg: '2',
+    },
+    {
+        src: 'сцена36.jpg',
+        nameImg: '2',
+    },
+    {
+        src: 'сцена37.png',
+        nameImg: '2',
+    },
+    {
+        src: 'сцена38.jpg',
+        nameImg: '2',
+    },
+    {
+        src: 'сцена39.jpg',
+        nameImg: '2',
+    },
+    {
+        src: 'сцена40.jpg',
+        nameImg: '2',
+    },
+    {
+        src: 'сцена41.png',
+        nameImg: '2',
+    },
+    {
+        src: 'сцена42.jpg',
+        nameImg: '2',
+    },
+    {
+        src: 'сцена43.jpg',
+        nameImg: '2',
+    },
+    {
+        src: 'сцена44.jpg',
+        nameImg: '2',
+    },
+    {
+        src: 'сцена45.png',
+        nameImg: '2',
+    },
+    {
+        src: 'сцена46.jpg',
+        nameImg: '2',
+    },
+    {
+        src: 'сцена47.jpg',
+        nameImg: '2',
+    },
+    {
+        src: 'сцена48.jpg',
+        nameImg: '2',
+    },
+    {
+        src: 'сцена49.png',
+        nameImg: '2',
+    },
+    {
+        src: 'сцена50.jpg',
+        nameImg: '2',
+    },
+    {
+        src: 'сцена51.jpg',
+        nameImg: '2',
+    },
+    {
+        src: 'сцена52.jpg',
+        nameImg: '2',
+    },
+    {
+        src: 'сцена53.png',
+        nameImg: '2',
+    },
+    {
+        src: 'сцена54.jpg',
+        nameImg: '2',
+    },
+    {
+        src: 'сцена55.jpg',
+        nameImg: '2',
+    },
+    {
+        src: 'сцена56.jpg',
+        nameImg: '2',
+    },
+    {
+        src: 'сцена57.png',
+        nameImg: '2',
+    },
+    {
+        src: 'сцена58.jpg',
+        nameImg: '2',
+    },
+    {
+        src: 'сцена59.jpg',
+        nameImg: '2',
+    },
+    {
+        src: 'сцена60.jpg',
+        nameImg: '2',
+    },
+    {
+        src: 'сцена61.png',
+        nameImg: '2',
+    },
+    {
+        src: 'сцена62.jpg',
+        nameImg: '2',
+    },
+    {
+        src: 'сцена63.jpg',
+        nameImg: '2',
+    },
+    {
+        src: 'сцена64.jpg',
+        nameImg: '2',
+    },
+    {
+        src: 'сцена65.png',
+        nameImg: '2',
+    },
+    {
+        src: 'сцена66.jpg',
+        nameImg: '2',
+    },
+    {
+        src: 'сцена67.jpg',
+        nameImg: '2',
+    },
+    {
+        src: 'сцена68.jpg',
+        nameImg: '2',
+    },
+    {
+        src: 'сцена69.png',
+        nameImg: '2',
+    },
+    {
+        src: 'сцена70.jpg',
+        nameImg: '2',
+    },
+    {
+        src: 'сцена71.jpg',
+        nameImg: '2',
+    },
+    {
+        src: 'сцена72.jpg',
+        nameImg: '2',
+    },
+    {
+        src: 'сцена73.png',
+        nameImg: '2',
+    },
+    {
+        src: 'сцена74.jpg',
+        nameImg: '2',
+    },
+    {
+        src: 'сцена75.jpg',
+        nameImg: '2',
+    },
+    {
+        src: 'сцена76.jpg',
+        nameImg: '2',
+    },
+    {
+        src: 'сцена77.png',
+        nameImg: '2',
+    },
+    {
+        src: 'сцена78.jpg',
+        nameImg: '2',
+    },
+    {
+        src: 'сцена79.jpg',
+        nameImg: '2',
+    },
+    {
+        src: 'сцена80.jpg',
+        nameImg: '2',
+    },
+    {
+        src: 'сцена81.png',
+        nameImg: '2',
+    },
+    {
+        src: 'сцена82.jpg',
+        nameImg: '2',
+    },
+    {
+        src: 'сцена83.jpg',
+        nameImg: '2',
+    },
+    {
+        src: 'сцена84.jpg',
+        nameImg: '2',
+    },
+    {
+        src: 'сцена85.png',
+        nameImg: '2',
+    },
+    {
+        src: 'сцена86.jpg',
+        nameImg: '2',
+    },
+    {
+        src: 'сцена87.jpg',
+        nameImg: '2',
+    },
+    {
+        src: 'сцена88.jpg',
+        nameImg: '2',
+    },
+    {
+        src: 'сцена89.png',
+        nameImg: '2',
+    },
+    {
+        src: 'сцена90.jpg',
+        nameImg: '2',
+    },
+    {
+        src: 'сцена91.jpg',
+        nameImg: '2',
+    },
+    {
+        src: 'сцена92.jpg',
+        nameImg: '2',
+    },
+    {
+        src: 'сцена93.png',
+        nameImg: '2',
+    },
+    {
+        src: 'сцена94.jpg',
+        nameImg: '2',
+    },
+    {
+        src: './cцена95.jpg',
+        nameImg: '2',
+    },
+    {
+        src: './cцена96.jpg',
+        nameImg: '2',
+    },
+    {
+        src: './cцена97.jpg',
+        nameImg: '2',
+    },
+    {
+        src: 'сцена98.jpg',
+        nameImg: '2',
+    },
+    {
+        src: 'сцена99.png',
+        nameImg: '2',
+    },
+    {
+        src: 'сцена100.png',
+        nameImg: '2',
+    },
+    {
+        src: 'сцена101.png',
+        nameImg: '2',
+    },
+    {
+        src: 'сцена102.png',
+        nameImg: '2',
+    },
+    {
+        src: 'сцена103.png',
+        nameImg: '2',
+    },
+    {
+        src: 'сцена104.png',
+        nameImg: '2',
+    },
+    {
+        src: 'сцена105.png',
+        nameImg: '2',
+    },
+    {
+        src: 'сцена106.png',
+        nameImg: '2',
+    },
+    {
+        src: 'сцена107.png',
+        nameImg: '2',
+    },
+    {
+        src: 'сцена108.png',
+        nameImg: '2',
+    },
+    {
+        src: 'сцена109.png',
+        nameImg: '2',
+    },
+    {
+        src: './бекстейдж6.jpg',
+        nameImg: '2',
+    },
+];
+
+const img= document.querySelector('img');
+const button = document.querySelector('button');
+
+const setImg = (currentDataImg) => {
+    img.src = currentDataImg.src;
+    img.alt = currentDataImg.nameImg;
+}
+
+setImg(listImages[0]);
+
+button.addEventListener('click', onChangeImg);
+
+let count = 0;
+
+function onChangeImg() {
+    count += 1;
+    setImg(listImages[count]);
+}
